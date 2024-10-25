fb925327c822a86be7488726911df06259dcbc6f
Josh Bright <josh.bright@gmail.com>
2024-10-25 15:09:56 -0700

    Added agency list page

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def index(request):
     return render(request, "home/index.html", {})
+
+def agency_list(request):
+    agency_list = models.Agency.objects.all()
+    return render(request, "home/agency-list.html", {"agency_list": agency_list})
