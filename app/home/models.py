67e1da8f44dbdce513333e89ff7b469fe335e0d3
Josh Bright <josh.bright@gmail.com>
2024-11-21 16:39:07 -0800

    Added new '2x3' seat option

--- app/home/models.py
+++ app/home/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
 class Agency(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
 
+
 class Bus(models.Model):
     SEAT_CONFIG_CHOICES = (
         ("1x1", "1x1"),
         ("2x2", "2x2"),
+        ("2x3", "2x3"),
     )
     agency = models.ForeignKey(Agency, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, unique=True)
@@ -20,9 +23,11 @@ class Bus(models.Model):
     def __str__(self):
         return self.name
 
+
 class BusTrip(models.Model):
     bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
     departure_time = models.DateTimeField()
+
 
 class Seat(models.Model):
     bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
@@ -32,6 +37,7 @@ class Seat(models.Model):
 
     def __str__(self):
         return f"{self.row}-{self.column}"
+
 
 class SeatUserLink(models.Model):
     seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
