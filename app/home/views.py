434763a35f582e4e8662e98fc97ba6d5d0f8f1f3
Josh Bright <josh.bright@gmail.com>
2024-11-21 16:30:59 -0800

    We now render a row for the driver on the bus detail page

--- app/home/views.py
+++ app/home/views.py
@@ -3,13 +3,15 @@ from . import models
 from . import forms
 from . import utils
 
-# Create your views here.
+
 def index(request):
     return render(request, "home/index.html", {})
+
 
 def agency_list(request):
     agency_list = models.Agency.objects.all()
     return render(request, "home/agency-list.html", {"agency_list": agency_list})
+
 
 def add_agency(request):
     if request.method == "POST":
@@ -21,9 +23,11 @@ def add_agency(request):
         form = forms.AgencyForm()
     return render(request, "home/add-agency.html", {"form": form})
 
+
 def bus_list(request):
     bus_list = models.Bus.objects.all()
     return render(request, "home/bus-list.html", {"bus_list": bus_list})
+
 
 def add_bus(request):
     if request.method == "POST":
@@ -36,12 +40,25 @@ def add_bus(request):
         form = forms.BusForm()
     return render(request, "home/add-bus.html", {"form": form})
 
+
 def bus_detail(request, pk):
     bus_data = get_object_or_404(models.Bus, pk=pk)
     seat_list = models.Seat.objects.filter(bus=bus_data).order_by("row", "column")
     seat_data = utils.organize_seats_for_bus(bus_data, seat_list)
+    # How many empty seats we need next to the driver seat
+    row_count = len(seat_data[0]) - 1
     bus_trips = models.BusTrip.objects.filter(bus=bus_data).order_by("departure_time")
-    return render(request, "home/bus-detail.html", {"bus_data": bus_data, "seat_data": seat_data, "bus_trips": bus_trips})
+    return render(
+        request,
+        "home/bus-detail.html",
+        {
+            "bus_data": bus_data,
+            "seat_data": seat_data,
+            "bus_trips": bus_trips,
+            "row_count": range(row_count)
+        }
+    )
+
 
 def add_bus_trip(request, bus_pk):
     bus_data = get_object_or_404(models.Bus, pk=bus_pk)
@@ -58,6 +75,7 @@ def add_bus_trip(request, bus_pk):
     else:
         form = forms.BusTripForm()
     return render(request, "home/add-bus-trip.html", {"form": form})
+
 
 def bus_trip_detail(request, pk):
     trip_data = get_object_or_404(models.BusTrip, pk=pk)
